db857de6313b7a1e03aa8e9b022aec9904d046c8
Lior Deutsch <sliorde@gmail.com>
2020-04-19 23:39:14 +0300

    added documnetation to update.py

--- location-based-sim/update.py
+++ location-based-sim/update.py
@@ -1,31 +1,58 @@
+"""
+functions for updating the state of the world
+"""
 from person import Person
 from timing import datetime
 
 
-def update_people(people, policy, time, time_step):
+def move_people(people, policy, time, time_step):
+    """
+    move all people from site to site.
+    `people` is the collection of all `Person`s.
+    `time` is the current time.
+    `time_step' is the size of the time step, in minutes.
+    """
     for person in people:
         move_person(person, policy, time, time_step)
 
 
 def move_person(person: Person, policy, time: datetime, time_step):
+    """
+    move person to next location, taking into account also the policy.
+    `time` is the current time.
+    `time_step' is the size of the time step, in minutes.
+    """
+
     if person.is_in_dummy_site:
+        # if the person is currently in the dummy state, check if it is time to
+        # move it out of there, and if so - move it.
         if time >= person.next_site_time:
             new_site = person.next_site
             person.change_site(new_site)
         return
     else:
+
+        # `out` will have the result of the first commuting pattern to be
+        # executed. if no commuting pattern is executed, it will remain `None`.
         out = None
         for commuting_patten in person.commuting_patterns:
-            out = commuting_patten.apply(person.site, time, person.time_in_current_site, time_step)
+            out = commuting_patten.apply(person.site, time,
+                                         person.time_in_current_site, time_step)
             if out is not None:
                 new_site, next_site_time = out[0], out[1]
                 break
         if out is None:
+            # no commuting pattern was executed - don't move person.
             return
         else:
+            # a commuting pattern was executed.
             if new_site == person.site:
+                # if the new site equal the current site, don't do anything.
                 return
             elif next_site_time > time:
+                # if the destination time in the new site is in the future - go
+                # temporarily to the dummy site.
                 person.put_in_dummy_site(new_site, next_site_time)
             else:
+                # otherwise, go to the destination
                 person.change_site(new_site)
