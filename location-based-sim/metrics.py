9e1a67882928833abc7401d78ea8f1f2f960f1c8
Lior Deutsch <sliorde@gmail.com>
2020-04-20 02:04:11 +0300

    initial implementation for `MetricManager`

---
+++ location-based-sim/metrics.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from person import Person
+from sites import Site
+from timing import datetime
+
+
+class MetricManager:
+    def __init__(self, people: List[Person], sites: List[Site]):
+        self.people = people
+        self.sites = sites
+
+    def get_sir_proportions(self):
+        s = 0
+        i = 0
+        r = 0
+        for person in self.people:
+            if person.illness_degree > 0:
+                i += 1
+            elif person.immunity_degree > 0:
+                r += 1
+            else:
+                s += 1
+
+        tot = s + i + r
+        return s / tot, i / tot, r / tot
+
+    def show(self, time:datetime):
+        s,i,r = self.get_sir_proportions()
+        print(time)
+        print('S: {:6.2f}%   I: {:6.2f}%   R: {:6.2f}%'.format(s*100,i*100,r*100))
+        print('')
