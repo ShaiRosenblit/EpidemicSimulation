d79e220ca40d38df23c50f315927e6435b7f5546
Lior Deutsch <sliorde@gmail.com>
2020-04-20 01:37:59 +0300

    added update function, to calculate new state of people

--- location-based-sim/update.py
+++ location-based-sim/update.py
@@ -1,13 +1,15 @@
 """
 functions for updating the state of the world
 """
+import random
 from typing import List
 
 from person import Person
+from sites import Site
 from timing import datetime
 
 
-def move_people(people:List[Person], policy, time:datetime, time_step:float):
+def move_people(people: List[Person], policy, time: datetime, time_step: float):
     """
     move all people from site to site.
     `people` is the collection of all `Person`s.
@@ -18,7 +20,7 @@ def move_people(people:List[Person], policy, time:datetime, time_step:float):
         move_person(person, policy, time, time_step)
 
 
-def move_person(person: Person, policy, time:datetime, time_step:float):
+def move_person(person: Person, policy, time: datetime, time_step: float):
     """
     move person to next location, taking into account also the policy.
     `time` is the current time.
@@ -62,3 +64,71 @@ def move_person(person: Person, policy, time:datetime, time_step:float):
             else:
                 # otherwise, go to the destination
                 person.change_site(new_site)
+
+
+def update_people_status(sites: List[Site], policy, time_step: float):
+    """
+    update the status of all people, by looping over all sites and updating
+    for the people in each site.
+    `time_step' is the size of the time step, in minutes.
+    """
+    for site in sites:
+        update_people_status_for_site(site, policy, time_step)
+
+
+def update_people_status_for_site(site: Site, policy, time_step: float):
+    """
+    update the status of all people in given site.
+    `time_step' is the size of the time step, in minutes.
+    """
+    people = site.people
+    number_of_people = len(people)
+    if number_of_people == 0:
+        return
+
+    # the following code should be changed to be a realistic model of the chance
+    # if infection given the site and people properties.
+
+    # calculate several variables
+    number_of_ill_people = sum(
+        [person.illness_degree > 0 for person in people])
+    ratio_of_ill_people = number_of_ill_people / number_of_people
+    density = number_of_people / site.area
+    ratio_of_capacity = number_of_people / site.nominal_capacity
+
+    # from these variables, get a "score" for the site, where a high score
+    # means higher chance if infection
+    site_infecting_score = time_step * ratio_of_ill_people * density * \
+                           ratio_of_capacity \
+                           * site.dispersion_factor / 50.0
+
+    # for each person, calculate whether it got infected, or maybe even healed
+    for person in people:
+
+        # a probability for this specific person of getting infected
+        person_infecting_score = site_infecting_score * (
+                    1 - person.immunity_degree) * person.susceptibility_degree
+
+        # perform infection
+        if random.random() < person_infecting_score:
+            person.illness_degree = 1.0
+            if person.time_infected_minutes is None:
+                person.time_infected_minutes = 0.0
+
+        # if did not get infected
+        else:
+
+            # if already infected from before
+            if person.illness_degree > 0.0:
+
+                # chance of healing
+                if random.random() < 0.00001*time_step:
+
+                    # apply healing
+                    person.illness_degree = 0.0
+                    person.immunity_degree = 1.0
+                    person.time_infected_minutes = None
+
+                # if did not heal
+                else:
+                    person.time_infected_minutes += time_step
